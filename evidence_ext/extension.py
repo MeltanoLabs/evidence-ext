0102486099a2edb9d0ac94b79d29cb07de328f1e
Ken Payne <ken@meltano.com>
2023-02-09 13:21:11 +0000

    write config

--- evidence_ext/extension.py
+++ evidence_ext/extension.py
@@ -1,6 +1,7 @@
 """Meltano Evidence extension."""
 from __future__ import annotations
 
+import json
 import os
 import pkgutil
 import subprocess
@@ -33,6 +34,38 @@ class Evidence(ExtensionBase):
             sys.exit(1)
         self.npm = Invoker("npm")
         self.npx = Invoker("npx")
+
+    def _get_connection_duckdb(self):
+        return {
+            "database": "duckdb",
+            "credentials": {
+                "filename": os.environ["EVIDENCE_CREDENTIALS_FILENAME"],
+                "gitignoreDuckdb": os.environ.get(
+                    "EVIDENCE_CREDENTIALS_GITIGNORE_DUCKDB"
+                ),
+            },
+        }
+
+    def _get_config(self):
+        database = os.environ["EVIDENCE_DATABASE"]
+        if database == "duckdb":
+            return self._get_config_duckdb()
+        else:
+            raise KeyError(
+                f"Database connection {database} is not yet supported by evidence-ext."
+            )
+
+    def _write_config(self):
+        config_file = (
+            Path(self.evidence_home)
+            / ".evidence"
+            / "template"
+            / "evidence.settings.json"
+        )
+        config_file.parent.mkdir(parents=True, exist_ok=True)
+        config = self._get_config()
+        with config_file.open("r") as cf:
+            json.dump(config, cf)
 
     def initialize(self, force: bool):
         """Initialize a new project."""
@@ -72,9 +105,11 @@ class Evidence(ExtensionBase):
         )
 
     def build(self):
+        self._write_config()
         self.npm.run_and_log(*["install", "--prefix", self.evidence_home])
         self.npm.run_and_log(*["run", "build", "--prefix", self.evidence_home])
 
     def dev(self):
+        self._write_config()
         self.npm.run_and_log(*["install", "--prefix", self.evidence_home])
         self.npm.run_and_log(*["run", "dev", "--prefix", self.evidence_home])
